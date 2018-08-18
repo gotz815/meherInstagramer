4270eb2f5d44f6c97b9d2ebc8fc5b3759b0d0731
堂福大成 <>
2018-08-18 15:30:28 +0900

    Remove samples controller and routes

--- app/controllers/samples_controller.rb
+++
@@ -1,9 +0,0 @@
-class SamplesController < ApplicationController
-  def top
-  end
-
-  def result
-        @logs = [1,2,3,4,5,6,7]
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,2 @@
 Rails.application.routes.draw do
-  root "samples#top"
-  post "/result" => "samples#result"
 end
