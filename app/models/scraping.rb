f03ee3579d052a6e70064d143cfd3f99777b53c9
gotz815 <allthamoon@gmail.com>
2018-08-18 15:23:06 +0900

    api  導入

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -2,10 +2,6 @@ class Scraping
   def self.get_product(link)
     agent = Mechanize.new
     page = agent.get(link)
-    image_url = page.at('.entry-content img')[:src] if page.at('.entry-content img')
-    open_date = page.at('.date span').inner_text if page.at('.date span')
-
-    image_url = image_url
-    product.save
+    image_url = page.at('.entry-content img')[:src] if page.at('.KLB4h src')
   end
 end
